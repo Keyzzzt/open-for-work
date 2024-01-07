c02736870060678a54faaa1cd6ce954522d776c7
keizt <tallinnn@hotmail.com>
2024-01-07 19:42:06 +0200

    Refactoring

--- app/(admin)/dashboard/(routes)/pages/landing/(c)/(testimonials)/LandingTestimonialsAdmin.tsx
+++ app/(admin)/dashboard/(routes)/pages/landing/(c)/(testimonials)/LandingTestimonialsAdmin.tsx
@@ -1,15 +1,10 @@
 'use client'
+import { FC, useState } from 'react'
 import { Heading2 } from '@/components/text/Heading2'
 import { Paragraph } from '@/components/text/Paragraph'
 import { LandingTestimonials, TestimonialItem } from '.prisma/client'
-import { FC, useCallback, useEffect, useState } from 'react'
 import { EditSectionNav } from '@/app/(admin)/dashboard/(routes)/pages/about/(components)/EditSectionNav'
 import { TestimonialsUpdate } from '@/app/(admin)/dashboard/(routes)/pages/landing/(c)/(testimonials)/TestimonialsUpdate'
-import { IconButtonHover } from '@/components/IconButtonHover'
-import { CiEdit, CiTrash } from 'react-icons/ci'
-import { Alert } from '@/components/Alert'
-import toast from 'react-hot-toast'
-import { TestimonialsItemUpdate } from '@/app/(admin)/dashboard/(routes)/pages/landing/(c)/(testimonials)/TestimonialsItemUpdate'
 
 type Props = {
   section: LandingTestimonials
@@ -18,73 +13,9 @@ type Props = {
 
 export const LandingTestimonialsAdmin: FC<Props> = ({ section, items }) => {
   const [isOpen, setIsOpen] = useState(false)
-  const [loading, setLoading] = useState(false)
-
-  // Delete item
-  const [deleteAlert, setDeleteAlert] = useState(false)
-  const [confirmed, setConfirmed] = useState(false)
-  const [itemId, setItemId] = useState('')
-  const [itemToUpdate, setItemToUpdate] = useState<
-    TestimonialItem | undefined
-  >()
-
-  const onDelete = useCallback((id: string) => {
-    setDeleteAlert(true)
-    setItemId(id)
-  }, [])
-
-  const handleItemDelete = useCallback(async () => {
-    setLoading(true)
-
-    try {
-      const response = await fetch(
-        `/api/pages/landing/testimonials/${itemId}`,
-        {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      )
-
-      if (!response.ok) {
-        return toast.error('Failed to delete')
-      }
-
-      toast.success('Deleted')
-    } catch (err) {
-      toast.error('Failed to delete')
-    } finally {
-      setLoading(false)
-    }
-  }, [itemId])
-
-  useEffect(() => {
-    if (confirmed) {
-      handleItemDelete()
-      setConfirmed(false)
-    }
-  }, [handleItemDelete, setConfirmed, confirmed])
 
   return (
-    <div aria-labelledby='testimonial-heading' className='relative'>
-      {deleteAlert && (
-        <Alert
-          type='danger'
-          isOpen={deleteAlert}
-          onClose={setDeleteAlert}
-          onConfirm={setConfirmed}
-          titleText='Delete testimonial item'
-          contentText='Are you sure you want to delete this item?'
-          buttonText='Delete'
-        />
-      )}
-      <TestimonialsItemUpdate
-        isOpen={!!itemToUpdate}
-        onClose={() => setItemToUpdate(undefined)}
-        item={itemToUpdate}
-        clearItemToUpdate={() => setItemToUpdate(undefined)}
-      />
+    <div className='relative'>
       <TestimonialsUpdate
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
@@ -94,9 +25,6 @@ export const LandingTestimonialsAdmin: FC<Props> = ({ section, items }) => {
         disabled={section.disabled}
         onClick={() => setIsOpen(true)}
       />
-      <h2 id='testimonial-heading' className='sr-only'>
-        Testimonials Section
-      </h2>
       <div className='mx-auto max-w-2xl lg:max-w-none'>
         <Heading2 value={section.title} id='testimonial-heading' />
         <div className='mt-16 space-y-16 lg:grid lg:grid-cols-3 lg:gap-x-8 lg:space-y-0'>
@@ -123,22 +51,6 @@ export const LandingTestimonialsAdmin: FC<Props> = ({ section, items }) => {
                   {item.author}
                 </cite>
               </div>
-              <IconButtonHover
-                items={[
-                  {
-                    disabled: loading,
-                    className: 'mr-4',
-                    icon: <CiTrash size={28} />,
-                    onClick: () => onDelete(item.id),
-                  },
-                  {
-                    onClick: () => {
-                      setItemToUpdate(item)
-                    },
-                    icon: <CiEdit size={28} />,
-                  },
-                ]}
-              />
             </blockquote>
           ))}
         </div>
